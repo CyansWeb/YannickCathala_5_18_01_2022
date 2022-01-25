d1917a17684dd250aacf9d17c83e9c0580d29ed4
Yannick Cathala <cyansweb@gmail.com>
2022-01-25 19:36:26 +0100

    fetch + getProduct

--- front/js/script.js
+++ front/js/script.js
@@ -4,30 +4,30 @@ const url = "http://localhost:3000/api/products/";
 //Fonction GET pour afficher les produits.
 function getProducts() {
     fetch(url).then((data) =>
-        data.json()
-        .then((data) => {
-            for (let i = 0; i < data.length; i++) { //itération pour chaque produit du fichier JSON.
-                let imageUrl = data[i].imageUrl;
-                let name = data[i].name;
-                let price = data[i].price;
-                let id = data[i]._id;
-                let description = data[i].description;
-                let altTxt = data[i].altTxt;
+            data.json()
+            .then((data) => {
+                for (let i = 0; i < data.length; i++) { //itération pour chaque produit du fichier JSON.
+                    let imageUrl = data[i].imageUrl;
+                    let name = data[i].name;
+                    let price = data[i].price;
+                    let id = data[i]._id;
+                    let description = data[i].description;
+                    let altTxt = data[i].altTxt;
 
-                console.log(data[i]);
-                //afficher les produits via innerHTML
-                let items = `<a href="./product.html?id=${id}">
+                    /* console.log(data[i]); */
+                    //afficher les produits via innerHTML
+                    let items = `<a href="./product.html?id=${id}">
                           <article>
-                            <img src="${imageUrl}" alt="${fetch }">
+                            <img src="${imageUrl}" alt="${altTxt}">
                             <h3 class="productName">${name}</h3>
                             <p class="productDescription">${description}</p>
                             <p>Prix : ${price}€</p>
                           </article>
                         </a>`;
-                document.querySelector(".items").innerHTML += items;
-            }
-        })
-    );
+                    document.querySelector(".items").innerHTML += items;
+                }
+            })
+        )
+        .catch((err) => console.log(err));
 }
-
 getProducts();
